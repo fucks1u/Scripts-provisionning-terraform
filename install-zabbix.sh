3b5ec926f8c184a2ba798f13b7e426289386b2ef
Ariirau FUCKS <91054245+fucks1u@users.noreply.github.com>
2023-01-30 09:58:23 +0100

    Create install-zabbix.sh

---
+++ install-zabbix.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+bleu="\033[34m"
+   # A exécuter sur zab
+        echo "${bleu}************************************** \\ Installation des paquets Zabbix // ********************************************************"
+
+sudo apt-get update
+sudo apt-get -y install apache2 php php-mysql php-mysqlnd php-ldap php-bcmath php-mbstring php-gd php-pdo php-xml libapache2-mod-php wget
+sudo apt-get -y install mariadb-server mariadb-client curl
+
+        echo "${bleu}************************************** \\ Installation et configuration de Zabbix // *************************************************"
+
+wget https://repo.zabbix.com/zabbix/6.2/debian/pool/main/z/zabbix-release/zabbix-release_6.2-4%2Bdebian11_all.deb
+sudo dpkg -i zabbix-release_6.2-4+debian11_all.deb
+sudo apt-get update
+sudo apt-get -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
+sudo apt-get -y install locales-all
+sudo locale-gen en_US.UTF-8
+sudo service apache2 restart
+
+apt-get update
+echo "mysql-server mysql-server/root_password password password" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password password" | sudo debconf-set-selections
+sudo sed -i 's/# DBPassword=/DBPassword=password/' /etc/zabbix/zabbix_server.conf
+sudo sed -i '963i\date.timezone = "Europe/Paris"' /etc/php/7.4/cli/php.ini
+sudo sed -i '963i\date.timezone = "Europe/Paris"' /etc/php/7.4/apache2/php.ini
+
+        echo "${bleu}************************* \\ Ajouter une base de donneés Zabbix à l'utilisatuer zabbix (Mariadb) // *********************************"
+
+sudo mysql -u root -ppassword <<SQL_QUERY
+CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';
+SET GLOBAL log_bin_trust_function_creators = 1;
+SQL_QUERY
+
+        echo "${bleu}************************************* \\ Telechargement des dependances de Zabbix // *************************************************"
+
+sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | sudo mysql --default-character-set=utf8mb4 -uzabbix -ppassword zabbix
+
+        echo "${bleu}*********************************************** \\ Configuration Mariadb-2 // *********************************************************"
+
+sudo mysql -u root -ppassword <<SQL_QUERY
+SET GLOBAL log_bin_trust_function_creators = 0;
+SQL_QUERY
+
+        echo "${bleu}*************************************************** \\ Configuration-Gui-zabbix // *****************************************************"
+
+sudo touch /etc/zabbix/web/zabbix.conf.php
+sudo chmod 777 /etc/zabbix/web/zabbix.conf.php
+sudo cat <<"EOF" > /etc/zabbix/web/zabbix.conf.php
+<?php
+$ZBX_LANG = 'fr_FR';
+// Zabbix GUI configuration file.
+$DB['TYPE']                     = 'MYSQL';
+$DB['SERVER']                   = 'localhost';
+$DB['PORT']                     = '0';
+$DB['DATABASE']                 = 'zabbix';
+$DB['USER']                     = 'zabbix';
+$DB['PASSWORD']                 = 'password';
+// Schema name. Used for PostgreSQL.
+$DB['SCHEMA']                   = '';
+// Used TLS connection.
+$DB['ENCRYPTION']               = false;
+$DB['KEY_FILE']                 = '';
+$DB['CERT_FILE']                = '';
+$DB['CA_FILE']                  = '';
+$DB['VERIFY_HOST']              = false;
+$DB['CIPHER_LIST']              = '';
+// Use IEEE754 compatible value range 64-bit Numeric
+$DB['VAULT']                    = '';
+$DB['VAULT_URL']                = '';
+$DB['VAULT_DB_PATH']            = '';
+$DB['VAULT_TOKEN']              = '';
+$DB['VAULT_CERT_FILE']          = '';
+$DB['VAULT_KEY_FILE']           = '';
+$DB['DOUBLE_IEEE754']           = true;
+// PHP Time zone
+$PHP_TZ = 'Europe/Paris';
+// User  webserver
+$ZBX_WEB_USER = 'www-data';
+// nom de la base Zabbix hostname/IP
+$ZBX_SERVER_NAME                = 'supervision_zabbix';
+// Identifiant par defaut de Zabbix
+$ZBX_USER = 'Admin';
+$ZBX_PASSWORD = 'zabbix';
+$IMAGE_FORMAT_DEFAULT   = IMAGE_FORMAT_PNG;
+EOF
+
+sudo chmod 600 /etc/zabbix/web/zabbix.conf.php
+sudo chown www-data:www-data /etc/zabbix/web/zabbix.conf.php
+
+        echo "${bleu}*********************************************** \\ Redemarre tout les services // ******************************************************"
+sudo service apache2 restart
+sudo service mariadb restart
+sudo service zabbix-server restart
+sudo service zabbix-agent restart
+sudo systemctl enable zabbix-server zabbix-agent apache2
+
+
+
