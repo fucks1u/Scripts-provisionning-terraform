173cb6dbf1f7d68b59d67c6e300a90361fc6e0af
Ariirau FUCKS <91054245+fucks1u@users.noreply.github.com>
2023-03-07 21:58:13 +0100

    Update install-zabbix.sh

--- install-zabbix.sh
+++ install-zabbix.sh
@@ -2,24 +2,24 @@
 
 bleu="\e[1;34m"
 
-bdd="34.107.111.77"
-zab="35.246.188.16"
+#Identifiant
 pass="passzabbix"
 
-        echo -e "${bleu}************************************** \\ Installation des paquets Zabbix // ********************************************************"
+#Ip
+bdd="34.107.111.77"
+zab="35.246.188.16"
 
+        echo -e "${bleu}************************************** \\ Installation des paquets Zabbix // ********************************************************"
 sudo apt-get update
 sudo apt-get -y install apache2 php php-mysql php-mysqlnd php-ldap php-bcmath php-mbstring php-gd php-pdo php-xml libapache2-mod-php wget
 
         echo -e "${bleu}************************************** \\ Installation et configuration de Zabbix // *************************************************"
-
 wget https://repo.zabbix.com/zabbix/6.2/debian/pool/main/z/zabbix-release/zabbix-release_6.2-4%2Bdebian11_all.deb -O zabbix-release6-2.deb
 sudo dpkg -i zabbix-release6-2.deb
 
         echo -e "${bleu}************************************** \\ Installation Zabbix server, frontend, agent  // ****************************************"
 sudo apt update
 sudo apt-get -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
-
 
         echo -e "${bleu}************************************** \\ Installation de la langue  // ****************************************"
 sudo apt-get -y install locales-all
@@ -30,16 +30,14 @@ sudo apt-get update
 
 sudo sed -i "s/# DBHost=localhost/DBHost=$bdd/" /etc/zabbix/zabbix_server.conf
 sudo sed -i "s/# DBPassword=/DBPassword=$pass/" /etc/zabbix/zabbix_server.conf
+
 sudo sed -i '963i\date.timezone = "Europe/Paris"' /etc/php/7.4/cli/php.ini
 sudo sed -i '963i\date.timezone = "Europe/Paris"' /etc/php/7.4/apache2/php.ini
 
-        echo -e "${bleu}**************************** \\ Ajouter une base de donneés Zabbix à l'utilisateur zabbix (Mariadb) // **********************************"
-
-#sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | sudo mysql --default-character-set=utf8mb4 -h ${bdd} -u zabbix -p${passzabbix} zabbix
+        echo -e "${bleu}**************************** \\ Ajouter une base de données Zabbix à l'utilisateur zabbix (Mariadb) // **********************************"
 sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | sudo mysql -h $bdd -u zabbix -p$pass zabbix
 
-        echo -e "${bleu}*************************************************** \\ Configuration-Gui-zabbix // *****************************************************"
-
+        echo -e "${bleu}*************************************************** \\ Configuration Gui-Zabbix // *****************************************************"
 sudo touch /etc/zabbix/web/zabbix.conf.php
 sudo chmod 777 /etc/zabbix/web/zabbix.conf.php
 sudo cat <<"EOF" > /etc/zabbix/web/zabbix.conf.php
@@ -82,20 +80,18 @@ EOF
 sudo sed -i "s/ip_database/$bdd/" /etc/zabbix/web/zabbix.conf.php
 sudo sed -i "s/pass_database/$pass/" /etc/zabbix/web/zabbix.conf.php
 
-    echo -e "${bleu}*************************************** \\ supprimer la page par defaut index.html // ******************"
-    
+    echo -e "${bleu}*************************************** \\ Suppression de la page par defaut index.html // ******************"
 sudo rm -rf /var/www/html/index.html
 sudo sed -i 's#DocumentRoot /var/www/html#DocumentRoot /usr/share/zabbix#g' /etc/apache2/sites-available/000-default.conf
 
 sudo chmod 600 /etc/zabbix/web/zabbix.conf.php
 sudo chown www-data:www-data /etc/zabbix/web/zabbix.conf.php
 
-
-        echo -e "${bleu}*********************************************** \\ Redemarre tout les services // ******************************************************"
-
+        echo -e "${bleu}*********************************************** \\ Redémarrage des services // ******************************************************"
 sudo service apache2 restart
 sudo service zabbix-server restart
 sudo service zabbix-agent restart
+
 sudo systemctl enable zabbix-server zabbix-agent apache2
 
 
